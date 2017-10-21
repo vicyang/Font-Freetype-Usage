2b5927e3fa40e32b4c0f4432039fd830e754dfa3
vicyang <523066680@163.com>
2017-10-21 14:10:01 +0800

    Signed-off-by: vicyang <523066680@163.com>

--- GL_Render/tools/LoadData.pl
+++ GL_Render/tools/LoadData.pl
@@ -1,16 +1,10 @@
 =info
     提取Outlines(轮廓信息)
 =cut
-use autodie;
-use utf8;
-use Encode;
-use Data::Dump qw/dump/;
-use Data::Dumper;
 
 BEGIN
 {
     use Storable;
-    use File::Slurp;
     use IO::Handle;
     STDOUT->autoflush(1);
 }
