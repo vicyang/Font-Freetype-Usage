9beea5aa37d4ed7c789a5d7b29823266032cdaf8
Vulkan <523066680@163.com>
2018-11-27 20:49:14 +0800

    枚举有效字符

--- Simple/enumerate_chars.pl
+++ Simple/enumerate_chars.pl
@@ -1,18 +1,28 @@
 use Font::FreeType;
+use Encode;
+use File::Slurp;
+use File::Basename qw/basename/;
 
-my $filename = "C:/Windows/Fonts/msyh.ttf";
+my $buff = "";
+my $ft = "trebuc";
+my $filename = "C:/Windows/Fonts/${ft}.ttf";
 my $face = Font::FreeType->new->face($filename);
+my $iter = 0;
 
 $face->foreach_char(
     sub 
     {
         if (defined $_->name )
         {
-            printf("%d\t%s\n", $_->char_code, $_->name);
+            printf "%d\t%s\n", $_->char_code, $_->name;
+            $buff .= encode('utf8', chr($_->char_code));
+            $buff .="\n" if $iter++ % 80 == 0;
         }
         else
         {
-            printf("%s\n", $_->char_code);
+            printf "Out: %s\n", $_->char_code;
         }
     }
 );
+
+write_file( basename(__FILE__, ".pl") ."_${ft}.txt" , $buff );
